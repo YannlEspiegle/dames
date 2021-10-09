d9b9e9010212b96291a79bb41bf57a5821cf8acc
Yann Didier <yann.didier54@gmail.com>
2021-10-09 17:22:00 +0200

    + deplacement/selection, + optimisation images

---
+++ pictures.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import pygame as pg
+
+PIECES = {
+    1: pg.image.load("assets/pion_blanc.png"),
+    2: pg.image.load("assets/pion_noir.png"),
+    10: pg.image.load("assets/dame_blanche.png"),
+    20: pg.image.load("assets/dame_noire.png"),
+}
